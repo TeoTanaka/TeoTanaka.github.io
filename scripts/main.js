2b34fdcbf11ba65f6cde77912e8a751021b8b590
TeoTanaka <144719988+TeoTanaka@users.noreply.github.com>
2024-04-24 16:31:04 +0000

    did test 1-4

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+console.log("Test 1");
+for (let i = 0; i < 10; i++) {
+    console.log(i*i);
+}
+console.log("Test2");
+for (let i = 5; i>0;i--){
+    console.log(i);
+}
+console.log("Blastoff!");
+console.log("Test3");
+for (let i = 0; i< 50; i+=2){
+    console.log(i);
+}
+let t = 0;
+console.log("Test4");
+for (let i =0; i<100; i++){
+    
+    t+=i
+}
+console.log(t);
+console.log("Test5")
+for (let i =0; i>10; i++) {
+    console.log(i);
+}
