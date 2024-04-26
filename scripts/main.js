1c1684ef867ce50167cc4490a8ae994b9eb36ad0
TeoTanaka <144719988+TeoTanaka@users.noreply.github.com>
2024-04-26 16:50:40 +0000

    finished assignment #2

--- scripts/main.js
+++ scripts/main.js
@@ -27,3 +27,61 @@ for (let i =0; i<10; i++) {
     }
     console.log(j);
 }
+
+console.log("Assignment #2");
+
+let arr = [3,1,4,1,5];
+console.log("task 1");
+for (let i =0; i<arr.length; i++) {
+    console.log(arr[i]);
+}
+
+console.log("task2");
+
+for (let i =0; i<arr.length; i++) {
+    let j = i+1;
+    console.log(arr.at(j*-1));
+}
+
+console.log("task3");
+
+let arr2 = [];
+
+for (let i =0; i<10; i++) {
+    let j = i+1;
+    arr2.push(j*j*j);
+}
+console.log(arr2);
+
+console.log("task4");
+
+let arr3 = [];
+let numPrev1 = 1;
+let numPrev2 = 0;
+let store = 0;
+
+for (let i =0; i<10; i++) {
+    
+    numPrev2 = numPrev1;
+    numPrev1 = store; 
+    
+    arr3.push(numPrev1+numPrev2);
+    store = numPrev1+numPrev2;
+}
+console.log(arr3);
+
+console.log("task5");
+
+let arr4 = [];
+let arrLength = arr.length;
+
+for (let i =0; i<arrLength; i++) {
+    arr4.push(arr.pop());
+}
+console.log(arr4);
+
+
+
+
+
+
